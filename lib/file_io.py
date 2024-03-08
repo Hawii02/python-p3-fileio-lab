dc133399c3e728c8676690fa2b0efbe6557f6ae5
leon <onditileon21@gmail.com>
2024-03-08 12:54:23 +0300

    completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,24 @@
+# def write_file(file_name, file_content):
+#    with open(f'lib/{file_name}.txt', mode ='w', encoding = 'utf-8') as file:
+#     file.write(file_content)
+
+# def append_file(file_name, append_content):
+#     with open(f'lib/{file_name}.txt', mode='a', encoding='utf-8') as file:
+#         file.write(append_content)
+
+# def read_file(file_name):
+#     with open(f'lib/{file_name}.txt', encoding='utf-8') as file:
+#         content = file.read()
+#     return content
+
 def write_file(file_name, file_content):
-    pass
+    with open(str (file_name) + '.txt' , 'w')as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(str (file_name) + '.txt', 'a')as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(str (file_name)+ '.txt', 'r')as f:
+        return f.read()
